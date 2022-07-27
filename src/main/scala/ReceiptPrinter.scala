356e06134c5140b621a6376e3cc9604d353d125a
Joe Thornton <thornton.jp@gmail.com>
2022-07-27 11:16:20 +0100

    small ammendment to how receipt calls cafeDetails

--- src/main/scala/ReceiptPrinter.scala
+++ src/main/scala/ReceiptPrinter.scala
@@ -19,7 +19,7 @@ class ReceiptPrinter(val cafe: CafeDetails, var order: Map[String, Int] = Map())
    * - the VAT (20% of total price)
    */
   def receipt: String = {
-    cafeDetails
+    s"""${cafeDetails}"""
   }
 
   private[this] def cafeDetails: String = {
